7821aa510034d17150a2e1c321e1cd038d58d8b1
Interagus <adityan.sk@hotmail.com>
2021-06-05 21:57:11 +0530

    Final Commit

--- fwall.py
+++ fwall.py
@@ -2,81 +2,27 @@
 import pydivert # Python
 from urllib.parse import urlparse, parse_qs
 from rules import verifyString
+import datetime
+from colorama import init, Fore, Back, Style;
 
-
-def parsePacketPayload(payload,connected):
-    parseList = []
-    payload_str = str(payload)
-    payload_str = payload_str + "`"
-    i=2
-    while i < len(payload_str):
-        if payload_str[i] == "\\" and i+1 < len(payload_str) and payload_str[i+1] == "x":
-            k=0
-            sstr = ""
-            sstr = sstr +"0x"
-            i = i+2
-            while i < len(payload_str) and k < 2:
-                sstr = sstr+ payload_str[i]
-                k = k + 1
-                i = i+1
-            parseList.append(sstr)
-        else:
-            sstr = ""
-            if payload_str[i] == "\\":
-                if payload_str[i+1] == "t":
-                    sstr = sstr + '\t'
-                elif payload_str[i+1] == "r":
-                    sstr = sstr + '\r'
-                else:
-                    sstr = sstr + '\n'
-                i = i+2
-            while i < len(payload_str) and payload_str[i] != "\\":
-                sstr = sstr + payload_str[i]
-                i = i+1
-            parseList.append(sstr)
-    try:
-        parseList[0] = ord(parseList[0])
-    except:
-        parseList[0] = int(str(parseList[0]),16)
-    for i in range(1,len(parseList)):
-        try:
-            parseList[i] = int(str(parseList[i]),16)
-        except:
-            continue
-    packetDetails = {}
-    packetDetails['Length'] = parseList[0] + parseList[1] + parseList[2]
-    packetDetails['Number'] = parseList[3]
-
-    if not connected :
-        packetDetails['ConnectionDetails'] = {}
-        packetDetails['ClientInfo'] = parseList[4:35]
-        packetDetails['ConnectionDetails']['Username'] = parseList[36]
-        packetDetails['ConnectionDetails']['Schema'] = parseList[39]
-        packetDetails['ConnectionDetails']['Password'] = parseList[41]
-        packetDetails['ConnectionDetails']['ClientName'] = parseList[47]
-        packetDetails['ConnectionDetails']['ServerHost'] = parseList[50]
-        connected = True
-    else:
-       packetDetails['Query'] = parseList[5]
-
-    return packetDetails
-
-def parseHTTTPPacket(payload):
+def parseHTTTPPacket(payload, log_file):
+    payload_params = {}
     payload_str = str(payload).split("\\n")
-    payload_cookie = payload_str[-3].split("; ")
+    if len(payload_str) > 2:
+        payload_cookie = payload_str[-3].split("; ")
     if payload_str[0][2] == "G":
         try:
-            payload_params = parse_qs(urlparse(payload_str[0][payload_str[0].index(" ")+1:payload_str[0].index(" ",payload_str[0].index(" ")+1)]).query)
+            payload_params = {**payload_params,**parse_qs(urlparse(payload_str[0][payload_str[0].index(" ")+1:payload_str[0].index(" ",payload_str[0].index(" ")+1)]).query)}
         except:
-            payload_params = payload_str[0]
+            payload_params = {**payload_params, **payload_str[0]}
     elif payload_str[0][2] == "P":
-        payload_params = parse_qs(payload_str[-1][:-2])
+        payload_params = {**payload_params, **parse_qs(payload_str[-1][:-2])}
     else:
         return
+    payload_params = {**payload_params, **{"Cookie": payload_cookie[-2:]}}
     for param in list(payload_params.values()):
         for string in param:
-            print(string)
-            if verifyString(string):
+            if verifyString(string,log_file):
                 return True
     return False
     
@@ -84,11 +30,36 @@ def parseHTTTPPacket(payload):
 
 #  Capture only TCP packets to port 80, i.e. HTTP requests.
 print("Intercepting Packets at dstPort 80")
+init()
 
 with pydivert.WinDivert("tcp.DstPort == 80 and tcp.PayloadLength > 0") as w:
+    print("Waiting for Packet")
     for packet in w:
-        result = parseHTTTPPacket(packet.payload) 
-        print(result)    
-        if not result:
-            w.send(packet)
-#
+        try:        
+            log_file =  open("D:\Projects\ISM_Proj\IPSLogs.txt", "a+")
+            if not log_file or log_file.closed:
+                print("Unable to create/open log file. Exiting the Program")
+                exit(0)
+            timestamp = datetime.datetime.now() 
+            print("[" + Fore.BLUE+ timestamp.strftime("%c") +Style.RESET_ALL+"]" , end=' ')
+            log_file.write("["+timestamp.strftime("%c")+"] ")
+            log_file.write(str(packet.src_addr)+":"+str(packet.src_port)+" ")
+            print(str(packet.src_addr)+":"+str(packet.src_port)+" ")
+            result = parseHTTTPPacket(packet.payload, log_file)
+            if not result:
+                print(Fore.GREEN, "Safe", Style.RESET_ALL)
+                w.send(packet)
+            else: 
+                print(Fore.RED,result, Style.RESET_ALL)    
+            log_file.write("****************************\n")  
+            log_file.flush()
+            log_file.close()  
+            if log_file.closed:
+                print("File Closed")
+            print(w)    
+        except:
+            print("Error Occured")
+            break
+        finally:
+            log_file.close()
+        
--- rules.py
+++ rules.py
@@ -1,49 +1,91 @@
 import re
-
-def f2(s):
-    x = re.search(r'(\s+)(OR|OR+|LIKE|CONTACT|VERSION|HOSTNAME|DATADIR|UUID)+(\s+)',s, re.IGNORECASE)
+from colorama import Fore, init, Style
+def f1(s, log_file):
+    x = re.search(r"[\n\s\W]*(\s|\W)*select[\n\s\W]*([a-zA-Z]*|.)[\n\s\W]*from(\s|\W)*", s, re.IGNORECASE)
     if x is None:
         return 0
     else:
-        print(x)
-        return 1
-def f3(s):
-    x = re.search(r"(\s+)UNION[^\n]*[\n\s]*SELECT(\s+)", s, re.IGNORECASE)
-    if x is None:
-        return 0
-    else:
-        print(x)
-        return 1
-def f4(s):
-    x = re.search(r"[?][\n\s]*=[\n\s]*UNION(\s+)", s, re.IGNORECASE)
-    if x is None:
-        return 0
-    else:
-        print(x)
-        return 1
-def f5(s):
-    x = re.search(r'(\s+)(ADD CONSTRAINT|ALTER COLUMN|ALTER TABLE|CREATE TABLE|CREATE VIEW|DROP COLUMN|DROP CONSTRAINT|DROP DATABASE|DROP TABLE|DROP VIEW|DROP INDEX|FOREIGN KEY|FULL OUTER JOIN|INNER JOIN|INSERT INTO SELECT|RIGHT JOIN|SELECT INTO|TRUNCATE TABLE)+(\s+)', s, re.IGNORECASE)
-    if x is None:
-        return 0
-    else:
-        print(x)
-        return 1
-def f6(s):
-    x = re.search(r"(\s+)DELETE(\s+)[a-zA-Z]+", s, re.IGNORECASE)
-    if x is None:
-        return 0
-    else:
-        print(x)
+        log_file.write(" ->"+s+"<- ")
+        print(Fore.RED, x, Style.RESET_ALL,end='')
         return 1
 
-def verifyString(s):
+def f2(s, log_file):
+    x = re.search(r'(\s|\W)+(OR|OR+|LIKE|RLIKE|CONTACT|VERSION|HOSTNAME|DATADIR|UUID)+(\s|\W)+',s, re.IGNORECASE)
+    if x is None:
+        return 0
+    else:
+        log_file.write(" ->"+s+"<- ")
+        print(Fore.RED, x, Style.RESET_ALL)
+        return 1
+def f3(s, log_file):
+    x = re.search(r"(\s|\W)+UNION[^\n]*[\n\s]*SELECT(\s|\W)+", s, re.IGNORECASE)
+    if x is None:
+        return 0
+    else:
+        log_file.write(" ->"+s+"<- ")
+        print(Fore.RED, x, Style.RESET_ALL)
+        return 1
+def f4(s, log_file):
+    x = re.search(r"(\s|\W)*(\w)*(\s|\W)*=[\n\s\W]*UNION(\s|\W)+", s, re.IGNORECASE)
+    if x is None:
+        return 0
+    else:
+        log_file.write(" ->"+s+"<- ")
+        print(Fore.RED, x, Style.RESET_ALL)
+        return 1
+def f5(s, log_file):
+    x = re.search(r'(\s|\w)+(ORDER BY|EXTRACTVALUE|ADD CONSTRAINT|ALTER COLUMN|ALTER TABLE|CREATE TABLE|CREATE VIEW|ORDER BY|DROP COLUMN|DROP CONSTRAINT|DROP DATABASE|DROP TABLE|DROP VIEW|DROP INDEX|FOREIGN KEY|FULL OUTER JOIN|INNER JOIN|INSERT INTO SELECT|RIGHT JOIN|SELECT INTO|TRUNCATE TABLE)+(\s)+', s, re.IGNORECASE)
+    if x is None:
+        return 0
+    else:
+        log_file.write(" ->"+s+"<- ")
+        print(Fore.RED, x, Style.RESET_ALL)
+        return 1
+def f6(s, log_file):
+    x = re.search(r"(\s|\W)+DELETE(\s|\W)+[a-zA-Z]+", s, re.IGNORECASE)
+    if x is None:
+        return 0
+    else:
+        log_file.write(" ->"+s+"<- ")
+        print(Fore.RED, x, Style.RESET_ALL)
+        return 1
+def f7(s, log_file):
+    x = re.search(r"(\s|\W)*(SLEEP|COUNT).[0-9]+.#(\s|\W)*", s, re.IGNORECASE)
+    if x is None:
+        return 0
+    else:
+        log_file.write(" ->"+s+"<- ")
+        print(Fore.RED, x, Style.RESET_ALL)
+        return 1
+def f8(s, log_file):
+    x = re.search(r"(\s|\W)*(AND|OR|NOT|\W)(\s|\W)+[\w]+=[\w]+(\s|\W)*", s, re.IGNORECASE) 
+    if x is None:
+        return 0
+    else:
+        log_file.write(" ->"+s+"<- ")
+        print(Fore.RED, x, Style.RESET_ALL)
+        return 1
+def f9(s, log_file):
+    x = re.search(r"(\s|\W)*(AND|OR|NOT)(\s|\W)+(\w)+(\s|\W)+(WHERE|HAVING|FROM)(\s|\W)*", s, re.IGNORECASE)
+    if x is None:
+        return 0
+    else:
+        log_file.write(" ->"+s+"<- ")
+        print(Fore.RED, x, Style.RESET_ALL)
+        return 1
+def verifyString(s, log_file):
     ctr = 0
-    ctr += f2(s)
-    ctr += f3(s)
-    ctr += f4(s)
-    ctr += f5(s)
-    ctr += f6(s)
+    ctr += f1(s, log_file)
+    ctr += f2(s, log_file)
+    ctr += f3(s, log_file)
+    ctr += f4(s, log_file)
+    ctr += f5(s, log_file)
+    ctr += f6(s, log_file)
+    ctr += f7(s, log_file)
+    ctr += f8(s, log_file)
+    ctr += f9(s, log_file)
     if(ctr>=1):
-        print("sqli detected")
+        print(Fore.YELLOW, "Sqli Attempt Detected", Style.RESET_ALL)
+        log_file.write(" --SQLI Attempt Detected-- ")
         return True
     return False
